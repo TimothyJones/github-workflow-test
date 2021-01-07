eefa9f666004a1de056f04af4f544ca1027143d9
Timothy Jones <timothy.l.jones@gmail.com>
2021-01-07 22:56:28 +1100

    fix(release): Correct release script so it will  run

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 echo "release"
 RELEASE_NOTES="$(cat CHANGELOG.md | awk 'BEGIN { flag=0 } /^###? \[/ { if (flag == 0) { flag=1 } else { flag=2 }; next } flag == 1')"
 echo "$RELEASE_NOTES"
 
+npm install
 npm run release
 
 echo "$GITHUB_CONTEXT"
