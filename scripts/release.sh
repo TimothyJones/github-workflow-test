7cb22ba092a11b6c48597ef490b0d2eb4752477a
Timothy Jones <timothy.l.jones@gmail.com>
2021-01-07 22:13:44 +1100

    fix(workflow): Print context in releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,3 +3,5 @@
 echo "release"
 RELEASE_NOTES="$(cat CHANGELOG.md | awk 'BEGIN { flag=0 } /^###? \[/ { if (flag == 0) { flag=1 } else { flag=2 }; next } flag == 1')"
 echo "$RELEASE_NOTES"
+
+echo "$GITHUB_CONTEXT"
